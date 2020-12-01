c12cff5a43f354387cd8081adfefc0618377d37d
R. van Elst <vanelst@dejongduke.nl>
2020-12-01 12:13:31 +0100

    More work on the OS X runner config. Install gnu sed and use that, now the test scripts work.

--- test/disk/unwritable/test.sh
+++ test/disk/unwritable/test.sh
@@ -20,8 +20,13 @@ fi
 chmod 700 disk-unwritable.img
 rm disk-unwritable.img
 
-sed -i -e 's$/[^ ]*/DiskFile.cpp$DiskFile.cpp$g' \
-    -e 's/line [0-9]*/line L/g' -e '/$Id/d' axp.log
+# OS X github runner has weiro sed version, use gnu one there.
+SED=/usr/bin/sed
+if [[ -f "/usr/local/opt/gnu-sed/libexec/gnubin/sed" ]]; then
+  SED=/usr/local/opt/gnu-sed/libexec/gnubin/sed
+fi
+
+${SED} -i -e 's$/[^ ]*/DiskFile.cpp$DiskFile.cpp$g' -e 's/line [0-9]*/line L/g' -e '/$Id/d' axp.log
 
 echo -n -e '\033[1;31m'
 diff -c axp_correct.log axp.log && echo -e '\033[1;32mdiff clean\033[0m'
